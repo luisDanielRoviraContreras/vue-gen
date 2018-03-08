2eed60fa56f19549f611773de9349c65f1f90a1a
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2018-03-08 18:48:59 -0300

    Generate component using templates

--- index.js
+++ index.js
@@ -2,22 +2,34 @@
 'use strict';
 
 const program = require('commander');
-const files = require('./files');
-const clear = require('clear');
+const fse = require('fs-extra')
+const fs = require('fs');
 
-const componentsPath = './src/components'
-
-if (files.directoryExists(componentsPath)) {
-    process.exit();
-}
+const componentsPath = `${__dirname}/src/components`;
+fse.ensureDirSync(componentsPath)
 
 const generate = (name, options) => {
-    console.log(name);
+    
+    const templateType = options.single ? 'single' : 'split';
+    const templatePath = `${__dirname}/templates/${templateType}`;
+
+    const newComponentPath = `${componentsPath}/${name}`;
+    fse.ensureDirSync(newComponentPath)
+    
+    const filesToCreate = fs.readdirSync(templatePath);
+
+    filesToCreate.forEach(file => {
+      
+      const templateFilePath = `${templatePath}/${file}`;
+      const templateFileContent = fs.readFileSync(templateFilePath, 'utf8');
+
+      const newComponentFile = `${newComponentPath}/${file}`;
+      fse.outputFileSync(newComponentFile, templateFileContent)
+    });
 }
 
 program
     .arguments('<name>')
     .option('-s, --single', 'Componente Single-File')
-    .version('0.0.1')
     .action(generate);
 program.parse(process.argv);
